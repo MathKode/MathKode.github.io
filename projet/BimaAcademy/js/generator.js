ef333b57960caf3ad8b12877829c7894a4649815
BiMathAx <61063210+MathKode@users.noreply.github.com>
2025-05-31 10:09:55 +0000

    Nouveau Système PDF + Correction syntaxe
    Nouveau système de lecture de pdf pour être adaptée à toutes les plateformes embed -> PDF.JS

--- projet/BimaAcademy/js/generator.js
+++ projet/BimaAcademy/js/generator.js
@@ -1,4 +1,3 @@
-
 const site_up = `
 <!DOCTYPE html>
 <html lang="fr">
@@ -111,111 +110,121 @@ function copyCode(button) {
 </script>
 `;
 
-
 function generate() {
-    cours = document.getElementById('cours').value
+    cours = document.getElementById("cours").value;
     const balise_all = {
         "[TITLE]": '<h1 class="exo-2-200 title">',
-        "[/TITLE]": '</h1>',
-    
+        "[/TITLE]": "</h1>",
+
         "[SUBTITLE]": '<h2 class="exo-2-200 subtitle">',
-        "[/SUBTITLE]": '</h2>',
-    
+        "[/SUBTITLE]": "</h2>",
+
         "[DESCRIPTION]": '<p class="exo-2-200 description">',
-        "[/DESCRIPTION]": '</p>',
-    
+        "[/DESCRIPTION]": "</p>",
+
         "[YOUTUBE]": '<iframe class="video" src="',
-        "[/YOUTUBE]": '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    
+        "[/YOUTUBE]":
+            '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+
         "[VIDEO]": '<video class="video" src="',
         "[/VIDEO]": '" controls></video>',
-    
+
         "[DIVIDER]": '<div class="divider"></div>',
-    
+
         "[JUMP]": '<div class="little_jump"></div>',
-    
+
         "[LINKTEXT]": '<a href="', // on parse txt:%:link séparément
-        "[/LINKTEXT]": '</a>',
+        "[/LINKTEXT]": "</a>",
 
         ":%:": '">',
-    
-        "[PDF]": '<embed class="pdf" src="',
-        "[/PDF]": '" type="application/pdf">',
-    
+
+        //"[PDF]": '<embed class="pdf" src="',
+        //"[/PDF]": '" type="application/pdf">',
+
+        "[PDF]":
+            '<iframe class="pdf" src="https://mozilla.github.io/pdf.js/web/viewer.html?file=',
+        "[/PDF]": '" ></iframe>',
+        //<iframe clas="pdf" src="https://mozilla.github.io/pdf.js/web/viewer.html?file=https://mathkode.github.io/projet/BimaAcademy/cours/ece%20physique/ECE_Nico.pdf" width="100%" height="600px"></iframe>
+
         "[LATEX]": '<span class="latex">\\[ ',
-        "[/LATEX]": ' \\]</span>',
-    
-        "[ARTICLE]": '<article class="article-content exo-2-200"><p>',  // contenu en <p>...</p> ligne par ligne
-        "[/ARTICLE]": '</p></article>',
-        "[A]" : '</p><p>',
-    
+        "[/LATEX]": " \\]</span>",
+
+        "[ARTICLE]": '<article class="article-content exo-2-200"><p>', // contenu en <p>...</p> ligne par ligne
+        "[/ARTICLE]": "</p></article>",
+        "[A]": "</p><p>",
+
         "[CODE]": `<div class="code-block">
         <button class="copy-btn hubballi-regular" onclick="copyCode(this)">Copier</button>
         <pre><code class="code">`,
         "[/CODE]": `</code></pre>
     </div>`,
 
-        "https://www.youtube.com/watch?v=":"https://www.youtube.com/embed/",
-        "https://youtu.be/" : "https://www.youtube.com/embed/",
+        "https://www.youtube.com/watch?v=": "https://www.youtube.com/embed/",
+        "https://youtu.be/": "https://www.youtube.com/embed/",
     };
-    Object.keys(balise_all).forEach(balise => {
-        cours = cours.split(balise).join(balise_all[balise])
+    Object.keys(balise_all).forEach((balise) => {
+        cours = cours.split(balise).join(balise_all[balise]);
     });
     //cas particuler, les articles
-    console.log(cours)
+    console.log(cours);
 
-    resultat = site_up + cours + site_down
+    resultat = site_up + cours + site_down;
 
-    
-
-    document.getElementById('cours_section').innerHTML = resultat
-    MathJax.typesetPromise() //Reload le latex
+    document.getElementById("cours_section").innerHTML = resultat;
+    MathJax.typesetPromise(); //Reload le latex
 
     //Copier dans le presse papier
-    navigator.clipboard.writeText(resultat)
+    navigator.clipboard.writeText(resultat);
     //alert('résultat copié dans le presse papier')
 
     //Créer un fichier et le dowload
-        
+
     // Création du Blob (fichier texte)
-    const blob = new Blob([resultat], { type: 'text/plain' });
-        
+    const blob = new Blob([resultat], { type: "text/plain" });
+
     // Création de l'URL du Blob
     const url = URL.createObjectURL(blob);
-        
+
     // Création du lien de téléchargement
-    const a = document.createElement('a');
+    const a = document.createElement("a");
     a.href = url;
-    a.download = 'index.html';  // Nom du fichier
+    a.download = "index.html"; // Nom du fichier
     document.body.appendChild(a);
-        
+
     // Simule le clic sur le lien pour déclencher le téléchargement
     a.click();
-        
+
     // Nettoie le DOM en supprimant le lien
     document.body.removeChild(a);
-        
+
     // Libère l'URL après le téléchargement
     URL.revokeObjectURL(url);
-    
-    return resultat
+
+    return resultat;
 }
 
 function json_generate() {
-    i1 = prompt('Nom du dossier [PATH] :','')
-    i2 = prompt('Nom du cours [NAME] :','')
-    i3 = prompt('Description du cours [DESCRIPTION] :','')
-    i4 = prompt('Auteur [Author] :','Mathis KREMER')
-    var ladate=new Date()
-    i5 = prompt(`Date du jour [${ladate.getDate()+"/"+(ladate.getMonth()+1)+"/"+ladate.getFullYear()}]`,ladate.getDate()+"/"+(ladate.getMonth()+1)+"/"+ladate.getFullYear())
-    tag_ls = []
-    ok=true
+    i1 = prompt("Nom du dossier [PATH] :", "");
+    i2 = prompt("Nom du cours [NAME] :", "");
+    i3 = prompt("Description du cours [DESCRIPTION] :", "");
+    i4 = prompt("Auteur [Author] :", "Mathis KREMER");
+    var ladate = new Date();
+    i5 = prompt(
+        `Date du jour [${ladate.getDate() + "/" + (ladate.getMonth() + 1) + "/" + ladate.getFullYear()}]`,
+        ladate.getDate() +
+            "/" +
+            (ladate.getMonth() + 1) +
+            "/" +
+            ladate.getFullYear(),
+    );
+    tag_ls = [];
+    ok = true;
     while (ok) {
-        i = prompt('Tag [TAG] et écrire  STOP  pour arrêter :','STOP')
-        if (i=='STOP') {
-            ok=false 
+        i = prompt("Tag [TAG] et écrire  STOP  pour arrêter :", "STOP");
+        if (i == "STOP") {
+            ok = false;
         } else {
-            tag_ls.push(i)
+            tag_ls.push(i);
         }
     }
     //Mise en forme
@@ -224,44 +233,44 @@ function json_generate() {
     "description" : "${i3}",
     "author": "${i4}",
     "date":"${i5}",
-    "tags" : [`
-    tag_ls.slice(0,-1).forEach(tag => {
-        result_json = result_json + `\n                "${tag}",`
+    "tags" : [`;
+    tag_ls.slice(0, -1).forEach((tag) => {
+        result_json = result_json + `\n                "${tag}",`;
     });
-    result_json = result_json + `\n            "${tag_ls.slice(-1)[0]}"`
-    result_json = result_json + `
+    result_json = result_json + `\n            "${tag_ls.slice(-1)[0]}"`;
+    result_json =
+        result_json +
+        `
             ]
-}`
-    alert(result_json)
-    console.log(result_json)
-    navigator.clipboard.writeText(result_json)
+}`;
+    alert(result_json);
+    console.log(result_json);
+    navigator.clipboard.writeText(result_json);
 
-    fichier_content = `{\n${result_json}\n}`
+    fichier_content = `{\n${result_json}\n}`;
 
     //Créer un fichier et le dowload
-        
+
     // Création du Blob (fichier texte)
-    const blob = new Blob([fichier_content], { type: 'text/plain' });
-        
+    const blob = new Blob([fichier_content], { type: "text/plain" });
+
     // Création de l'URL du Blob
     const url = URL.createObjectURL(blob);
-        
+
     // Création du lien de téléchargement
-    const a = document.createElement('a');
+    const a = document.createElement("a");
     a.href = url;
-    a.download = 'index.json';  // Nom du fichier
+    a.download = "index.json"; // Nom du fichier
     document.body.appendChild(a);
-        
+
     // Simule le clic sur le lien pour déclencher le téléchargement
     a.click();
-        
+
     // Nettoie le DOM en supprimant le lien
     document.body.removeChild(a);
-        
+
     // Libère l'URL après le téléchargement
     URL.revokeObjectURL(url);
-
-    
 }
 
-console.log('fichier js chargé')
+console.log("fichier js chargé");
